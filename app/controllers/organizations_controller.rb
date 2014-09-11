3cf2a2424c4e6322ddc7359a282d14425857ce03
Diego Medina <d.medina1290@gmail.com>
2014-09-11 18:14:41 -0400

    Added new Organizations form

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -7,4 +7,22 @@ class OrganizationsController < ApplicationController
     @organization = Organization.find(params[:id])
   end
 
+  def new
+    @organization = Organization.new
+  end
+
+  def create
+    @organization = Organization.new(organization_params)
+    if @organization.save
+      redirect_to(@organization)
+    else
+      render('new')
+    end
+  end
+
+private
+
+  def organization_params
+    params.require(:organization).permit(:name, :description)
+  end
 end
--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,5 @@
 class Organization < ActiveRecord::Base
   has_and_belongs_to_many :people
   has_and_belongs_to_many :projects
+  validates_presence_of :name, :description
 end
