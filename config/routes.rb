962d17064fdcb3b61086850e6673b7c9cc075e63
Jessica Burns <jburns@neboweb.com>
2014-09-14 20:35:20 -0400

    Indentation to spaces

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, skip: [:sessions], controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-	devise_scope :user do
-	  get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-	end
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
-	root 'welcome#index'
+  root 'welcome#index'
 
   resources :projects
   resources :people
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :user do
-  	email { "test@test.com" }
-  	password { "12345678" }
+    email { "test@test.com" }
+    password { "12345678" }
   end
 end
