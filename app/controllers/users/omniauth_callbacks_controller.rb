b8691f691f5c07452610cc8a3a1f02092030fde3
Jessica Burns <jburns@neboweb.com>
2014-09-14 20:31:45 -0400

    Change indent to spaces

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
     if @user.persisted?
       sign_in_and_redirect(@user, event: :authentication)
-     	flash[:success] = "Welcome, #{@user.name}!" if is_navigational_format?
+      flash[:success] = "Welcome, #{@user.name}!" if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       flash[:danger] = "We're sorry, but we couldn't log you in."
@@ -15,10 +15,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
   def from_omniauth(auth)
     User.where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-	    user.email = auth.info.email
-	    user.password = Devise.friendly_token[0,20]
-	    user.name = auth.info.name
-	    user.image = auth.info.image
-	  end
-	end
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+      user.image = auth.info.image
+    end
+  end
 end
