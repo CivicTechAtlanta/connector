2285259f0083787ddd9c3dbefcc84d4d02dd47c9
Diego Medina <d.medina1290@gmail.com>
2014-10-05 22:47:36 -0400

    corrections from pull request.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,20 +4,19 @@ class ProjectsController < ApplicationController
   before_filter :verify_project_creator, only: [:destroy]
 
   def has_access_to_update?
-    project.people.include? current_user.person
+    project.people.include?(current_user.person)
   end
 
   def verify_user_belongs_to_project
     unless has_access_to_update?
-      redirect_to('/')
+      redirect_to root_path
       flash[:danger] = "You must be a member of the project to edit it."
     end
   end
 
   def verify_project_creator
-    @project = project
-    unless @project.people.first == current_user.person
-      redirect_to @project
+    unless project.people.first == current_user.person
+      redirect_to project
     end
   end
 
@@ -37,10 +36,10 @@ class ProjectsController < ApplicationController
     @project = Project.new(project_params)
     if @project.save
       @project.people << current_user.person
-      redirect_to (@project)
+      redirect_to @project
       flash[:success] = "Project successfully created!"
     else
-      render('new')
+      render 'new'
       flash[:danger] = "Please fill in all fields."
     end
   end
@@ -51,20 +50,17 @@ class ProjectsController < ApplicationController
 
   def update
     if project.update(project_params)
+      redirect_to project
       flash[:success] = "Project successfully updated!"
     else
+      render 'edit'
       flash[:danger] = "We're sorry, your information could not be updated. Name and description are required fields."
     end
-    redirect_to project
   end
 
   def destroy
-    @project = project
-    if @project.destroy
-      flash[:success] = "Project successfully deleted!"
-    else
-      flash[:danger] = "The project could not be deleted. Only the project creator can delete the project."
-    end
+    project.destroy
+    flash[:success] = "Project successfully deleted!"
     redirect_to projects_path
   end
 
@@ -75,7 +71,7 @@ class ProjectsController < ApplicationController
   end
 
   def project
-    projects.includes(:people).find(params[:id])
+    @project ||= projects.includes(:people).find(params[:id])
   end
 
   def project_params
