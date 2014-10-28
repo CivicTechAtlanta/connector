71482109209c6ddd69becc89a11f7c8b96e19042
Jessica Burns <jburns@neboweb.com>
2014-10-27 20:46:56 -0400

    Remove image field from projects

--- db/migrate/20141028001436_add_fields_to_projects.rb
+++ db/migrate/20141028001436_add_fields_to_projects.rb
@@ -1,6 +1,5 @@
 class AddFieldsToProjects < ActiveRecord::Migration
   def change
-    add_column :projects, :image, :string
     add_column :projects, :short_description, :text
     add_column :projects, :link, :string
   end
