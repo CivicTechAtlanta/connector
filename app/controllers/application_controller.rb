8c728dea52e00a554ad04aec3df6f6ad21ff4c66
Stephen Bussey <steve.bussey@salesloft.com>
2014-11-23 18:15:02 -0500

    add comments to projects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_person
+
+  def current_person
+    current_user.try!(:person)
+  end
+
   def after_sign_in_path_for(resource)
     projects_path
   end
