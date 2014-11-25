c55746abd03a56110069d1668e3a5fd060d77905
Stephen Bussey <steve.bussey@salesloft.com>
2014-11-25 18:22:53 -0500

    add in omniauth failure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,8 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(resource)
     projects_path
   end
+
+  def new_session_path(scope)
+    projects_path
+  end
 end
