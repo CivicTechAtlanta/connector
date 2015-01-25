e20822d38ed265fe972a54776e91354e987d3397
Stephen Bussey <steve.bussey@salesloft.com>
2015-01-24 19:34:35 -0500

    notify the project about the comment

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -17,5 +17,27 @@ RSpec.describe CommentsController, :type => :controller do
       post :create, comment: "Test", project_id: project.id
       expect(response).to redirect_to(project_path(project))
     end
+
+    it "doesn't send any emails" do
+      expect {
+        post :create, comment: "Test", project_id: project.id
+      }.not_to change{ ActionMailer::Base.deliveries.size }
+    end
+
+    context "with other people on the project" do
+      let(:person1) { FactoryGirl.create(:person, user: FactoryGirl.create(:user)) }
+      let(:person2) { FactoryGirl.create(:person, user: FactoryGirl.create(:user)) }
+
+      before(:each) {
+        project.people << person1
+        project.people << person2
+      }
+
+      it "sends 2 emails" do
+        expect {
+          post :create, comment: "Test", project_id: project.id
+        }.to change{ ActionMailer::Base.deliveries.size }.by(2)
+      end
+    end
   end
 end
