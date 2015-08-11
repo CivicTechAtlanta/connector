449ca2dab7335564a863ccd6ca895f46973cb15e
Stephen Bussey <steve.bussey@salesloft.com>
2015-08-10 20:45:19 -0400

    display the next event on the homepage

--- config/initializers/time.rb
+++ config/initializers/time.rb
@@ -1,1 +1,6 @@
 Time::DATE_FORMATS[:comment] = "%m-%d-%Y"
+Time::DATE_FORMATS[:event] = lambda { |time|
+  day_format = ActiveSupport::Inflector.ordinalize(time.day)
+  time = time.in_time_zone('Eastern Time (US & Canada)')
+  time.strftime("%B #{day_format} %l:%M %p")
+}
--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     title { Faker::Lorem.words.join(" ") }
     description { Faker::Lorem.paragraph }
     url { Faker::Internet.url }
-    location { Faker::Lorem.words.join }
+    location { Faker::Lorem.words.join(" ") }
     start_at { Time.now }
     end_at { 2.hours.from_now }
   end
