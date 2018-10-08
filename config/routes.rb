990e2c875c49cf7f5b9845edeecf89c4f47cfb4b
Luigi Ray-Montanez <luigi@codeforatlanta.org>
2018-10-08 18:25:50 -0400

    Civic Spark Day redirect

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   get '/donate', to: redirect('https://secure.codeforamerica.org/page/contribute/donate-to-a-brigade-today?source_codes=codeforatlanta-redirect&brigade=Code%20for%20Atlanta')
   get '/data', to: redirect('https://docs.google.com/document/d/122GLuyEnhQbAFbOMGqnj4nSAD2OW13DpkAxsxsGJTsA/edit')
   get '/greenspace', to: redirect('https://docs.google.com/document/d/1t1ajdjHXcz4zI4joHMbgGSN8nCgvnPpyfPF2t2ZXp6g/edit?usp=sharing')
+  get '/spark', to: redirect('https://goo.gl/forms/bUQQW4zzD85GWQAf1')
 end
